deb3c84adbbdc168893e4f45a3c92f6763c0681f
Sjur G. Wroldsen <sjurgw@stud.ntnu.no>
2021-01-21 16:34:02 +0100

    UPDATE: pointcloud->rangeimage + ground plane segmentation for point clouds

--- include/tunnel_slam/feature_association.hpp
+++ include/tunnel_slam/feature_association.hpp
@@ -7,6 +7,7 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/range_image/range_image.h>
 
 class FeatureAssociation
 {
@@ -15,12 +16,25 @@ class FeatureAssociation
         ~FeatureAssociation(); // destructor method
         void pointCloud2Handler(const sensor_msgs::PointCloud2ConstPtr& pointCloud2Msg);
     private:
+        // ROS Members
         ros::NodeHandle nh_; // Defining the ros NodeHandle variable for registrating the same with the master
         ros::Subscriber subPointCloud2;
-        pcl::PointCloud<pcl::PointXYZ> _prevPointCloud2;
+        ros::Publisher pubFeatureCloud2;
+        ros::Publisher pubGroundPlaneCloud2;
 
+        // Previous point clouds
+        pcl::PointCloud<pcl::PointXYZ> _prevFeaturesCloud;
+        pcl::PointCloud<pcl::PointXYZ> _prevGroundPlaneCloud;
+
+        pcl::RangeImage _pointCloud2RangeImage(const pcl::PointCloud<pcl::PointXYZ> &cloud);
+
+        // Feature extraction
         void _findGroundPlane(const pcl::PointCloud<pcl::PointXYZ> &cloud, pcl::PointCloud<pcl::PointXYZ> &groundPlane, pcl::PointCloud<pcl::PointXYZ> &excludedGroundPlane);
         void _extractFeatures(const pcl::PointCloud<pcl::PointXYZ> &cloud, pcl::PointCloud<pcl::PointXYZ> &output);
+        void _publish(const pcl::PointCloud<pcl::PointXYZ> &groundPlaneCloud, const pcl::PointCloud<pcl::PointXYZ> &featureCloud);
+
+        //Transformation calculations
+        void _calculateTransformation(const pcl::PointCloud<pcl::PointXYZ> &groundPlaneCloud, const pcl::PointCloud<pcl::PointXYZ> &featureCloud);
 
 };
 #endif
--- src/feature_association.cpp
+++ src/feature_association.cpp
@@ -5,6 +5,8 @@
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/filters/passthrough.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/statistical_outlier_removal.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/keypoints/iss_3d.h>
 
@@ -12,18 +14,15 @@
 FeatureAssociation::FeatureAssociation(ros::NodeHandle &nh, ros::NodeHandle &pnh)
 {
     nh_ = nh;
-    std::cout<<"from Constructor \n";
-    // storing the values in the member variable
-    // get the parameters or configurations and store them in member variables
-    // initialize the publisher and subscribers
-    subPointCloud2 = nh.subscribe<sensor_msgs::PointCloud2>("/points2", 1, &FeatureAssociation::pointCloud2Handler, this);
+    subPointCloud2 = nh.subscribe<sensor_msgs::PointCloud2>("/points2", 32, &FeatureAssociation::pointCloud2Handler, this);
+    pubGroundPlaneCloud2 = nh.advertise<sensor_msgs::PointCloud2>("/groundPlanePointCloud", 32);
+    pubFeatureCloud2 = nh.advertise<sensor_msgs::PointCloud2>("/featurePointCloud", 32);
 }
 
 // Destructor method
 FeatureAssociation::~FeatureAssociation()
 {
-    std::cout<<"from Destructor \n";
-    // Free up the memory assigned from heap
+
 }
 
 void FeatureAssociation::pointCloud2Handler(const sensor_msgs::PointCloud2ConstPtr& pointCloud2Msg)
@@ -31,13 +30,20 @@ void FeatureAssociation::pointCloud2Handler(const sensor_msgs::PointCloud2ConstP
     pcl::PointCloud<pcl::PointXYZ> cloud;
     pcl::fromROSMsg(*pointCloud2Msg, cloud);
 
+    pcl::RangeImage rangeImage = _pointCloud2RangeImage(cloud);
+    std::cout << "RANGE IMAGE\n" << rangeImage << std::endl;
+
     pcl::PointCloud<pcl::PointXYZ> groundPlane;
     pcl::PointCloud<pcl::PointXYZ> excludedGroundPlane;
     _findGroundPlane(cloud, groundPlane, excludedGroundPlane);
+    std::cout << "EXCLUDED GROUND PLANE\n" << excludedGroundPlane << std::endl;
+    std::cout << "GROUND PLANE\n" << groundPlane << std::endl;  
 
-    pcl::PointCloud<pcl::PointXYZ> features;
-    _extractFeatures(excludedGroundPlane, features);
-    std::cout<<features<<std::endl;
+    pcl::PointCloud<pcl::PointXYZ> featuresCloud;
+    _extractFeatures(excludedGroundPlane, featuresCloud);
+    std::cout << "FEATURES CLOUD" << featuresCloud << std::endl;
+
+    _publish(groundPlane, featuresCloud);
 }
 
 void FeatureAssociation::_findGroundPlane(const pcl::PointCloud<pcl::PointXYZ> &cloud, pcl::PointCloud<pcl::PointXYZ> &groundPlane, pcl::PointCloud<pcl::PointXYZ> &excludedGroundPlane)
@@ -48,14 +54,14 @@ void FeatureAssociation::_findGroundPlane(const pcl::PointCloud<pcl::PointXYZ> &
     pcl::PassThrough<pcl::PointXYZ> pass(true);
     pass.setInputCloud(cloud.makeShared());
     pass.setFilterFieldName("z");
-    pass.setNegative(true);
-    pass.setFilterLimits(-2, 0);
+    pass.setNegative(false);
+    pass.setFilterLimits(-2, -0.1);
     pass.filter(*potentialGroundPoints);
     //std::cout << *excludedGround << std::endl;
 
-    pcl::IndicesConstPtr groundIndicesPtr = pass.getRemovedIndices();
+    pcl::IndicesConstPtr notGroundPtr = pass.getRemovedIndices();
 
-    //pcl::PointCloud<pcl::PointXYZ> potentialGroundPoints(cloud, *groundIndicesPtr);
+    //pcl::PointCloud<pcl::PointXYZ> potentialGroundPoints(cloud, *notGroundPtr);
     // Set up RANSAC MODEL
 
     pcl::SACSegmentation<pcl::PointXYZ> sacGroundPlane;
@@ -63,26 +69,42 @@ void FeatureAssociation::_findGroundPlane(const pcl::PointCloud<pcl::PointXYZ> &
     sacGroundPlane.setOptimizeCoefficients(true);
     sacGroundPlane.setModelType(pcl::SACMODEL_PLANE);
     sacGroundPlane.setMethodType(pcl::SAC_RANSAC);
-    sacGroundPlane.setDistanceThreshold(0.01);
+    sacGroundPlane.setDistanceThreshold(0.1);
 
     // Perform fit
-
     pcl::ModelCoefficients coefficients;
-    pcl::PointIndices::Ptr inliers (new pcl::PointIndices());
+    pcl::PointIndices::Ptr inliers(new pcl::PointIndices());
     sacGroundPlane.segment(*inliers, coefficients);
     
     // #TODO: Extract ground plane indices relative to original cloud and output ground plane 
 
-    groundPlane = pcl::PointCloud<pcl::PointXYZ>(cloud, inliers->indices);
-    pcl::PassThrough<pcl::PointXYZ> removeGroundPlaneFilter;
-    removeGroundPlaneFilter.setInputCloud(excludedGroundPlane.makeShared());
-    removeGroundPlaneFilter.setIndices(inliers);
-    removeGroundPlaneFilter.setNegative(true); // NOT ENOUGH, need to get inliers relative to cloud
-    //removeGroundPlaneFilter.filter();
+    groundPlane = pcl::PointCloud<pcl::PointXYZ>(*potentialGroundPoints, inliers->indices);
+
+    pcl::ExtractIndices<pcl::PointXYZ> removeGroundPlaneFilter;
+    removeGroundPlaneFilter.setInputCloud(cloud.makeShared());
+    removeGroundPlaneFilter.setIndices(notGroundPtr);
+    removeGroundPlaneFilter.setNegative(false);
+    removeGroundPlaneFilter.filter(excludedGroundPlane);
+
+    /*pcl::PointIndices::Ptr originalGroundPlaneIndices(new pcl::PointIndices());
+    for (auto it : inliers->indices){
+        originalGroundPlaneIndices->indices.emplace_back((*notGroundPtr)[it]);
+    }
+    //std::cout << "removed indices from original cloud: " << originalGroundPlaneIndices->indices.size() << std::endl;
+
+    pcl::ExtractIndices<pcl::PointXYZ> removeGroundPlaneFilter(true);
+    removeGroundPlaneFilter.setInputCloud(cloud.makeShared());
+    removeGroundPlaneFilter.setIndices(originalGroundPlaneIndices);
+    removeGroundPlaneFilter.setNegative(false);
+    removeGroundPlaneFilter.filter(excludedGroundPlane);*/
+
+    //std::cout << "Original Cloud\n " << cloud << std::endl;  
+
 }
 
 void FeatureAssociation::_extractFeatures(const pcl::PointCloud<pcl::PointXYZ> &cloud, pcl::PointCloud<pcl::PointXYZ> &output)
-{   
+{      
+
     pcl::PointCloud<pcl::Normal> normals;
     pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normalEstimator;
     normalEstimator.setInputCloud(cloud.makeShared());
@@ -99,3 +121,45 @@ void FeatureAssociation::_extractFeatures(const pcl::PointCloud<pcl::PointXYZ> &
     keypointDetector.compute(output);
 
 }
+
+void FeatureAssociation::_publish(const pcl::PointCloud<pcl::PointXYZ> &groundPlaneCloud, const pcl::PointCloud<pcl::PointXYZ> &featureCloud){
+    if (pubGroundPlaneCloud2.getNumSubscribers() > 0){
+        sensor_msgs::PointCloud2 msg;
+        pcl::toROSMsg(groundPlaneCloud, msg);
+        pubGroundPlaneCloud2.publish(msg);
+    }
+    if (pubFeatureCloud2.getNumSubscribers() > 0){
+        sensor_msgs::PointCloud2 msg;
+        pcl::toROSMsg(featureCloud, msg);
+        pubFeatureCloud2.publish(msg);
+    }
+}
+
+pcl::RangeImage FeatureAssociation::_pointCloud2RangeImage(const pcl::PointCloud<pcl::PointXYZ> &cloud){
+    
+    // SENSOR SPECIFIC
+
+    float angularResolutionX    = (float) (pcl::deg2rad(0.16));
+    float angularResolutionY    = (float) (pcl::deg2rad(1.25));
+    float maxAngleWidth         = (float)  (pcl::deg2rad((float)180));
+    float maxAngleHeight        = (float) (pcl::deg2rad((float)40));
+
+    // Navigation parameters 
+
+    Eigen::Affine3f sensorPose  = (Eigen::Affine3f)Eigen::Translation3f(0.0f, 0.0f, 0.0f);
+    pcl::RangeImage::CoordinateFrame coordinate_frame = pcl::RangeImage::LASER_FRAME;
+
+    float noiseLevel    = 0.00;
+    float minRange      = 0.0f;
+    int borderSize      = 1;
+
+    // Create Range Image
+
+    pcl::RangeImage rangeImage;
+    rangeImage.createFromPointCloud(cloud, angularResolutionX, angularResolutionY, maxAngleWidth, maxAngleHeight, sensorPose, coordinate_frame, noiseLevel, minRange, borderSize);
+    return rangeImage;
+
+}
+
+void FeatureAssociation::_calculateTransformation(const pcl::PointCloud<pcl::PointXYZ> &groundPlaneCloud, const pcl::PointCloud<pcl::PointXYZ> &featureCloud){}
+
